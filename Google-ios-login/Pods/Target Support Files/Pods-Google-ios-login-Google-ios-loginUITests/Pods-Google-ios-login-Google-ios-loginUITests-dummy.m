020593c5b9f94a40191a03921a95de616b3882fd
pan3800 <pan173434@gmail.com>
2024-09-25 16:39:30 +0900

    cocoapods 연동

---
+++ Google-ios-login/Pods/Target Support Files/Pods-Google-ios-login-Google-ios-loginUITests/Pods-Google-ios-login-Google-ios-loginUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Google_ios_login_Google_ios_loginUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Google_ios_login_Google_ios_loginUITests
+@end
---
+++ Google-ios-login/Pods/Target Support Files/Pods-Google-ios-loginTests/Pods-Google-ios-loginTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Google_ios_loginTests : NSObject
+@end
+@implementation PodsDummy_Pods_Google_ios_loginTests
+@end
