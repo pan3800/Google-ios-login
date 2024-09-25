4ea63a724fcaefc0c5fab4dd2d190338ac5258cb
pan3800 <pan173434@gmail.com>
2024-09-25 16:33:51 +0900

    프로젝트 생성

---
+++ Google-ios-login/Google-ios-login/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Google-ios-login
+//
+//  Created by 임채성 on 2024/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Google-ios-login/Google-ios-login/Google_ios_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_ios_loginApp.swift
+//  Google-ios-login
+//
+//  Created by 임채성 on 2024/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Google_ios_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
