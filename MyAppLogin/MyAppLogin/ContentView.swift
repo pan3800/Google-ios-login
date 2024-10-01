8dd6c726272544cef3267a54c81be0e0fb42e8fc
pan3800 <pan173434@gmail.com>
2024-10-01 18:22:05 +0900

    구글 소셜 로그인 구현 성공

--- MyAppLogin/MyAppLogin/ContentView.swift
+++ MyAppLogin/MyAppLogin/ContentView.swift
@@ -6,24 +6,13 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            
-            Image("KakaoTalk_Photo_2024-09-19-15-14-53")
-                .resizable()
-                .frame(width: 75, height: 75)
-                .clipShape(Circle())
-            
-            Text("Click in Login")
-            
-            Button {
-                print ( "Google 로그인 탭됨" )
-                authServiceModel.googleSignIn()
-            } label: {
-                Image("ios_light_sq_ctn")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 250, height: 150)
+            if authServiceModel.signState == .signIn {
+                MainView()
+            } else {
+                LoginView()
             }
         }
+        .padding()
     }
     
     struct ContentView_Previews: PreviewProvider {
---
+++ MyAppLogin/MyAppLogin/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  MyAppLogin
+//
+//  Created by 임채성 on 2024/10/01.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Google 소셜 로그인 성공")
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
